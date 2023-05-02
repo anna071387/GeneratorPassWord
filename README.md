be8fda5b37a065da791b06ef31caf2839cc6df72
Anna Gallagher <annagallagher713@gmail.com>
2023-05-01 21:42:49 -0600

    updated assets, colors, html structure, css structure, added readme file

--- README.md
+++ README.md
@@ -26,3 +26,10 @@ THEN a password is generated that matches the selected criteria
 WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
 ```
+
+
+
+
+
+Starter code Provided by:
+© 2023 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,74 @@
 // Assignment code here
+const characterAmountRange = document.getElementById
+('characterAmountRange')
+const characterAmountNumber = document.getElementById
+('characterAmountNumber')
+const includeUppercaseElement = document.getElementById
+('includeUppercase')
+const includeNumbersElement = document.getElementById
+('includeNumbers')
+const includeSymbolsElement = document.getElementById
+('includeSymbols')
+const form = document.getElementById('passwordGeneratorForm')
+
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRange.addEventListener('input', syncCharacterAmount)
+
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58,64))
+  .concat(arrayFromLowToHigh(91.96)).concat(arrayFromLowToHigh(123,126))
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+form.addEventListener('submit', e=> {
+  e.preventDefault()
+  const characterAmount = characterAmountNumber.value
+  const includeUppercase = includeUppercaseElement.checked 
+  const includeNumbers = includeNumbersElement.checked 
+  const includeSymbols = includeSymbolsElement.checked 
+  const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+})
 
-  passwordText.value = password;
-
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+  console.log(LOWERCASE_CHAR_CODES);
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function arrayFromLowToHigh(low,high) {
+  const array = {}
+  for (let i = low; i<= high; i++) {
+    array.push(i)
+  }
+  return array
+}
+
+
+function syncCharacterAmount(e) {
+  const value = e.target.value
+  characterAmountNumber.value = value
+  characterAmountRange.value = value
+}
+
+
+
+
+
+
+// // Write password to the #password input
+// function writePassword(length) {
+//   let password = '';
+//   const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()"
+//   let passwordLength = length;
+
+//   const array = new Uint32Array(length);
+//   self.crypto.getRandomValues(array);
+
+//   for (i = 0; i < length; i++) {
+//     password += chars[array[i] % chars.length];
+//   } 
+//   console.log(password);
+//   return password;
+
+// }
+
